14931aa5fa2c7da64c97e53019c58a322d536c63
Ankush1oo8 <154556772+Ankush1oo8@users.noreply.github.com>
2025-04-26 04:07:13 +0530

    Two Sum II - Input Array Is Sorted 9

---
+++ twoSumII_InputArrayIsSorted.java
@@ -0,0 +1,22 @@
+/*
+* solved it previously
+* got the idea 
+* this i was applying in siple two sum problem wher the array was not sorted
+*/
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l=0,r=numbers.length-1;
+        while(l<r){
+            if(numbers[l]+numbers[r]==target){
+                return new int [] {l+1,r+1};
+            }
+            else if(numbers[l]+numbers[r]>target){
+                r--;
+            }else{
+                l++;
+            }
+        }
+        return new int[] {-1,-1};
+    }
+}
