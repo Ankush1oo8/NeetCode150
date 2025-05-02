52a34edad0752be325b14f773350c354e319cc89
Ankush1oo8 <154556772+Ankush1oo8@users.noreply.github.com>
2025-05-03 00:24:07 +0530

    better way trapping rain water

--- trappingRainWater.java
+++ trappingRainWater.java
@@ -36,3 +36,31 @@ class Solution {
         return sum;
     }
 }
+
+//better ways using two pointer
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        int left=0;
+        int right=n-1;
+        int leftMax=height[0];
+        int rightMax=height[n-1];
+        int sum=0;
+        while(left<right){
+            if(height[left]<height[right]){
+                leftMax=Math.max(leftMax,height[left]);
+                if(leftMax-height[left]>0){
+                    sum+=leftMax-height[left];
+                }
+                 left++;   
+            }else{
+                rightMax=Math.max(rightMax,height[right]);
+                if(rightMax-height[right]>0){
+                    sum+=rightMax-height[right];
+                }
+                right--;   
+            }
+        }
+        return sum;
+    }
+}
